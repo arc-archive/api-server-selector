5873808425996c06c6ceaf1d5f1d348a5e1f5fd1
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2020-03-26 16:07:39 -0300

    feat: map server options and handle changes

--- src/api-server-selector.js
+++ src/api-server-selector.js
@@ -7,6 +7,28 @@ import { EventsTargetMixin } from '@advanced-rest-client/events-target-mixin/eve
  * An element to generate view model for server
  * elements from AMF model
  *
+ * This component receives an AMF model, and listens
+ * to navigation events to know which node's servers
+ * it should render.
+ * 
+ * When the selected server changes, it dispatches an `api-server-changed`
+ * event, with the following details:
+ * - Server value: the server id (for listed servers in the model), the URL
+ *    value (when custom URL is selected), or the value of the `anypoint-item`
+ *    component rendered into the extra slot
+ * - Selected type: `server` | `custom` | `extra`
+ *    - `server`: server from the AMF model
+ *    - `custom`: custom URL input change
+ *    - `extra`: extra slot's anypoint-item `value` attribute (see below)
+ * 
+ * Adding extra slot:
+ * This component renders a `slot` element to render anything the users wants
+ * to add in there. To enable this, sit the `extraOptions` value in this component
+ * to true, and render an element associated to the slot name `api-server-extra-slot`.
+ * The items rendered in this slot should be `anypoint-item` components, and have a
+ * `value` attribute. This is the value that will be dispatched in the `api-server-changed`
+ * event.
+ * 
  *
  *
  * @customElement
@@ -30,6 +52,14 @@ export class ApiServerSelector extends EventsTargetMixin(AmfHelperMixin(LitEleme
        * AMF model to be rendered
        */
       amf: { type: Object },
+      /**
+       * Holds the current servers to show in in the dropdown menu
+       */
+      _servers: { type: Array },
+      _selectedIndex: { type: Number },
+      _selectedValue: { type: String },
+      _endpointId: { type: String },
+      _methodId: { type: String },
     };
   }
 
@@ -46,6 +76,10 @@ export class ApiServerSelector extends EventsTargetMixin(AmfHelperMixin(LitEleme
     return this._servers;
   }
 
+  get selected() {
+    return this._selectedValue;
+  }
+
   set amf(model) {
     const old = this._amf;
     if (old === model) {
@@ -57,36 +91,6 @@ export class ApiServerSelector extends EventsTargetMixin(AmfHelperMixin(LitEleme
 
   get amf() {
     return this._amf;
-  }
-
-  get currentServer() {
-    return this._currentServer;
-  }
-
-  set currentServer(value) {
-    const old = this._currentServer;
-    if (value === old) {
-      return;
-    }
-    this._currentServer = value;
-  }
-
-  get serverId() {
-    return this._serverId;
-  }
-
-  set serverId(value) {
-    const old = this._serverId;
-    if (value === old) {
-      return;
-    }
-
-    this._serverId = value;
-    dispatchEvent(
-      new CustomEvent('api-server-changed', {
-        detail: { value: value },
-      }),
-    );
   }
 
   get methodId() {
@@ -115,64 +119,201 @@ export class ApiServerSelector extends EventsTargetMixin(AmfHelperMixin(LitEleme
     this._endpointId = value;
   }
 
-  /**
-   * Sets the selected server information
-   *
-   * @param {Object} selectedNodeParams The currently selected node parameters to set the servers for
-   * @param {String} selectedNodeParams.id The selected node ID where servers should be fetched
-   * @param {String} selectedNodeParams.type The selected node type where servers should be fetched
-   * @param {String} selectedNodeParams.endpointId The selected endpointID where servers should be fetched
-   * @param {?String} selectedNodeParams.serverId The currently selected server ID
-   */
-  set selected({ id, type, endpointId, serverId }) {
-    const old = this._serverId;
-    if (serverId === old) {
-      return;
-    }
-
-    if (type === 'method') {
-      this.methodId = id;
-    }
-    this.endpointId = endpointId;
-    this.serverId = serverId;
-    this.servers = this._getServers(endpointId, id);
-    this.currentServer =
-      this._getServer({ endpointId, methodId: id, id: serverId }) || this.servers[0];
-  }
-
   handleNavigationChange(e) {
     const { selected, type, endpointId } = e.detail;
-
     const serverDefinitionAllowedTypes = ['endpoint', 'method'];
     if (serverDefinitionAllowedTypes.indexOf(type) === -1) {
       return;
     }
+    const oldServers = this.servers;
+    this.updateServers({ id: selected, type, endpointId });
+    this._checkForSelectedChange(oldServers);
+  }
 
-    this.selected = { id: selected, type, endpointId, serverId: this.serverId };
+  _checkForSelectedChange(oldServers) {
+    if (!oldServers) {
+      oldServers = [];
+    }
+    if (this._selectedIndex === undefined || this._selectedIndex === null) {
+      return;
+    }
+    let newIndex;
+    let newValue = this._selectedValue;
+    const isModelServerSelected = this._selectedIndex < oldServers.length
+    if (isModelServerSelected) {
+      const indexInNewServers = this._getIndexOfServer(this._selectedValue, this.servers)
+      if (indexInNewServers > -1) {
+        newIndex = indexInNewServers;
+        // this._changeSelected({ selectedIndex: indexInNewServers, selectedValue: this._selectedValue })
+      } else {
+        newIndex = undefined;
+        newValue = undefined;
+        // this._resetSelectedServer();
+      }
+    } else {
+      const serverOffest = this.servers.length - oldServers.length;
+      newIndex = this._selectedIndex + serverOffest;
+      // this._changeSelected({ selectedIndex: newIndex, selectedValue: this._selectedValue })
+    }
+    this._changeSelected({ selectedIndex: newIndex, selectedValue: newValue })
+  }
+
+  _getIndexOfServer(serverId, servers) {
+    for (let i = 0; i < servers.length; i++) {
+      const server = servers[i];
+      if (this._getValue(server, '@id') === serverId) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  _resetSelectedServer() {
+    this._changeSelected({ selectedIndex: undefined, selectedValue: undefined });
+  }
+
+  _getServerValue(server) {
+    if (server) {
+      return this._getValue(server, '@id');
+    }
+    return undefined;
+  }
+
+  /**
+   * Update component's servers
+   * 
+   * @param {Object} selectedNodeParams The currently selected node parameters to set the servers for
+   * @param {String} selectedNodeParams.id The selected node ID where servers should be fetched
+   * @param {String} selectedNodeParams.type The selected node type where servers should be fetched
+   * @param {?String} selectedNodeParams.endpointId Optional endpoint id the method id belongs to
+   */
+  updateServers({ id, type, endpointId }) {
+    let methodId;
+    if (type === 'method') {
+      this.methodId = methodId = id;
+    }
+    if (type === 'endpoint') {
+      endpointId = id;
+    }
+    this.endpointId = endpointId;
+    this.servers = this._getServers({ endpointId, methodId });
+  }
+
+  /**
+   * Handler for the listbox's change event
+   * @param {CustomEvent} e 
+   */
+  handleSelectionChanged(e) {
+    const { selectedItem } = e.target
+    const { value } = e.detail
+    if (!selectedItem) {
+      return;
+    }
+    const selectedValue = selectedItem.getAttribute('value');
+    if (selectedValue === this._selectedValue) {
+      return;
+    }
+    const selectedIndex = value;
+    this._changeSelected({ selectedIndex, selectedValue });
+  }
+
+  /**
+   * 
+   * @param {Object} params Composed object
+   * @param {Number} params.selectedIndex The index of the selected item in the listbox
+   * @param {String} params.selectedValue The value of the selected item in the listbox
+   */
+  _changeSelected({ selectedIndex, selectedValue }) {
+    if (selectedIndex === this._selectedIndex && selectedValue === this._selectedValue) {
+      return;
+    }
+    this._selectedIndex = selectedIndex;
+    this._selectedValue = selectedValue;
+    dispatchEvent(
+      new CustomEvent('api-server-changed', {
+        detail: { value: selectedValue },
+      }),
+    );
+  }
+
+  /**
+   * Call the render functions for
+   * - Server options (from AMF Model)
+   * - Custom URL option
+   * - Extra slot
+   * @return {TemplateResult} The combination of all options
+   */
+  renderItems() {
+    return html`
+      ${this.renderServerOptions()}
+      ${this.renderCustomURLOption()}
+      ${this.renderExtraSlot()}
+    `;
+  }
+
+  renderServerOptions() {
+    const { servers } = this;
+    const key = this._getAmfKey(this.ns.aml.vocabularies.core.urlTemplate);
+    const toAnypointItem = (server) => {
+      const urlTemplate = this._getValue(server, key);
+      return html`
+      <anypoint-item value="${this._getServerValue(server)}">${urlTemplate}</anypoint-item>
+    `;
+    };
+    return servers ? servers.map(toAnypointItem) : [];
+  }
+
+  /**
+   * @return {TemplateResult} Custom URL `anypoint-item`
+   */
+  renderCustomURLOption() {
+    return html`
+      <anypoint-item value="custom">Custom URL</anypoint-item>
+    `;
+  }
+
+  /**
+   * Returns template result with `slot` element if
+   * `extraOptions` attribute is enabled, or undefined if
+   * it is not.
+   * @return {TemplateResult|undefined}
+   */
+  renderExtraSlot() {
+    const { extraOptions } = this;
+    if (extraOptions) {
+      return html`
+        <slot name="api-server-extra-slot"></slot>
+      `;
+    }
+    return undefined;
   }
 
   render() {
-    // TODO list in dropdown Sever values
+    // TODO
     // Add input/label with server baseURI considering when URL is editable and when it is readonly
-    // Add slot for custom server options inside dropdown and slot for additionalOptions section
+    const { _selectedIndex } = this
     return html`
-      <anypoint-dropdown>
-        <anypoint-listbox>
-          <anypoint-item>Server 1</anypoint-item>
-          <anypoint-item>Server 2</anypoint-item>
-          <anypoint-item>Custom URL</anypoint-item>
-        </anypoint-listbox>
-      </anypoint-dropdown>
+    <anypoint-dropdown-menu>
+      <label slot="label">Select server</label>
+      <anypoint-listbox
+        .selected="${_selectedIndex}"
+        @selected-changed="${this.handleSelectionChanged}"
+        slot="dropdown-content"
+        tabindex="-1"
+      >
+        ${this.renderItems()}
+      </anypoint-listbox>
+    </anypoint-dropdown>
     `;
   }
 
   _attachListeners(node) {
     super._attachListeners(node);
-    node.addEventListener('api-navigation-selection-changed', this.handleNavigationChange);
+    node.addEventListener('api-navigation-selection-changed', this.handleNavigationChange.bind(this));
   }
 
   _detachListeners(node) {
     super._detachListeners(node);
-    node.removeEventListener('api-navigation-selection-changed', this.handleNavigationChange);
+    node.removeEventListener('api-navigation-selection-changed', this.handleNavigationChange.bind(this));
   }
 }
