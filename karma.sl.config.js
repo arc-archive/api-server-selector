8d1ec176870fe5475f3ecc48c4fa6180fe6ad3c6
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2020-03-30 14:52:08 -0300

    test: update karma settings

--- karma.sl.config.js
+++ karma.sl.config.js
@@ -1,23 +1,19 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const merge = require('deepmerge');
-const slSettings = require('@advanced-rest-client/testing-karma-sl/sl-settings.js');
+const { slSettings } = require('@advanced-rest-client/testing-karma-sl');
 const createBaseConfig = require('./karma.conf.js');
 
-module.exports = config => {
+module.exports = (config) => {
   const slConfig = merge(slSettings(config), {
     sauceLabs: {
-      testName: 'api-authorization',
+      testName: 'api-resource-example-document',
     },
+    client: {
+      mocha: {
+        timeout: 10000
+      }
+    }
   });
-  slConfig.browsers = [
-    'SL_Chrome',
-    'SL_Chrome-1',
-    'SL_Firefox',
-    'SL_Firefox-1',
-    'SL_Safari',
-    // 'SL_Safari-1',
-    'SL_EDGE',
-  ];
   config.set(merge(createBaseConfig(config), slConfig));
   return config;
 };
